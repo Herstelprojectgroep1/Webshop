5b62eac33501cdb6ac397dfdb808b13ff5bb7e46
Moniquesbg <moniquesbg@gmail.com>
2022-03-04 16:06:11 +0100

    added products and discount in the database

--- database/webshop_windows.sql
+++ database/webshop_windows.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 03 mrt 2022 om 15:48
+-- Gegenereerd op: 04 mrt 2022 om 16:01
 -- Serverversie: 10.4.20-MariaDB
 -- PHP-versie: 8.0.9
 
@@ -28,41 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `discount` (
-  `discountID` int(10) NOT NULL,
-  `productID` int(10) NOT NULL,
+  `discountID` int(11) NOT NULL,
+  `productID` int(11) DEFAULT NULL,
   `discount` int(11) NOT NULL,
-  `newPrice` int(11) NOT NULL,
+  `newPrice` int(11) DEFAULT NULL,
   `beginDate` date NOT NULL,
   `endDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Tabelstructuur voor tabel `order`
+-- Gegevens worden geëxporteerd voor tabel `discount`
 --
 
-CREATE TABLE `order` (
-  `ID` int(10) NOT NULL,
-  `user_ID` int(10) NOT NULL,
-  `order_item_ID` int(10) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `track_and_trace` varchar(50) NOT NULL,
-  `order_date` date NOT NULL,
-  `delivery_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `order_item`
---
-
-CREATE TABLE `order_item` (
-  `ID` int(10) NOT NULL,
-  `product_ID` int(10) NOT NULL,
-  `order_ID` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `discount` (`discountID`, `productID`, `discount`, `newPrice`, `beginDate`, `endDate`) VALUES
+(34, 21, 25, 74, '2022-03-28', '2022-03-31'),
+(35, 28, 25, 224, '2022-03-28', '2022-03-31'),
+(36, 21, 25, 74, '2022-03-02', '2022-03-09'),
+(37, 28, 25, 224, '2022-03-02', '2022-03-09');
 
 -- --------------------------------------------------------
 
@@ -72,55 +54,27 @@ CREATE TABLE `order_item` (
 
 CREATE TABLE `product` (
   `productID` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` text NOT NULL,
-  `price` decimal(10,2) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
   `category` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Tabelstructuur voor tabel `role`
+-- Gegevens worden geëxporteerd voor tabel `product`
 --
 
-CREATE TABLE `role` (
-  `role_ID` int(10) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `role`
---
-
-INSERT INTO `role` (`role_ID`, `name`, `description`) VALUES
-(1, 'user', 'This role is meant for normal users (clients) that uses the webshop'),
-(2, 'admin', 'This role is meant for admin users'),
-(3, 'orderpicker', 'This role is meant for orderpickers');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `user`
---
-
-CREATE TABLE `user` (
-  `ID` int(10) NOT NULL,
-  `role_ID` int(10) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `date_of_birth` date NOT NULL,
-  `logged_in` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `user`
---
-
-INSERT INTO `user` (`ID`, `role_ID`, `username`, `email`, `password`, `date_of_birth`, `logged_in`) VALUES
-(1, 1, 'Monique_sabong', 'moniquesbg@gmail.com', '$2y$10$aw8k5.lxt4EjlTzAVqOTKej4DxYy.XB4mgSOE7lDABJOZnnrqfn2C', '2002-10-22', 0);
+INSERT INTO `product` (`productID`, `name`, `description`, `price`, `category`) VALUES
+(20, 'Roze badeend', 'Deze zeer kleine badeend is roze', 99, 'T'),
+(21, 'zwarte badeend', 'Deze zeer kleine badeend is zwart', 99, 'T'),
+(22, 'Gaius Kwakkus', 'Een badeend gekleed als een romein', 499, 'L'),
+(23, 'Tor Bjürn Anka', 'Een badeend gekleed als een viking', 499, 'L'),
+(24, 'Nero', 'Het is de romeinse keizer Nero', 399, 'M'),
+(25, 'Schaap', 'Hij weet niet of hij een schaap is of een badeend', 399, 'M'),
+(26, 'Badeend (standaard)', 'De standaard badeend die iedereen kent', 299, 'S'),
+(27, 'groende badeend', 'deze badeend is dus groen', 299, 'S'),
+(28, 'Glowie', 'Een kleine badeend die een lichtje is in het donker', 299, 'T'),
+(29, 'Engeltje', 'Heilige badeend', 250, 'S');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -133,39 +87,10 @@ ALTER TABLE `discount`
   ADD PRIMARY KEY (`discountID`);
 
 --
--- Indexen voor tabel `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `user_ID` (`user_ID`),
-  ADD KEY `order_item_ID` (`order_item_ID`);
-
---
--- Indexen voor tabel `order_item`
---
-ALTER TABLE `order_item`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `product_ID` (`product_ID`),
-  ADD KEY `order_ID` (`order_ID`);
-
---
 -- Indexen voor tabel `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`productID`);
-
---
--- Indexen voor tabel `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`role_ID`);
-
---
--- Indexen voor tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `role_ID` (`role_ID`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -175,61 +100,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT voor een tabel `discount`
 --
 ALTER TABLE `discount`
-  MODIFY `discountID` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `order`
---
-ALTER TABLE `order`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `order_item`
---
-ALTER TABLE `order_item`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `discountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `role`
---
-ALTER TABLE `role`
-  MODIFY `role_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT voor een tabel `user`
---
-ALTER TABLE `user`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Beperkingen voor geëxporteerde tabellen
---
-
---
--- Beperkingen voor tabel `order`
---
-ALTER TABLE `order`
-  ADD CONSTRAINT `order_item_ID` FOREIGN KEY (`order_item_ID`) REFERENCES `order_item` (`ID`),
-  ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Beperkingen voor tabel `order_item`
---
-ALTER TABLE `order_item`
-  ADD CONSTRAINT `order_ID` FOREIGN KEY (`order_ID`) REFERENCES `order` (`ID`),
-  ADD CONSTRAINT `product_ID` FOREIGN KEY (`product_ID`) REFERENCES `product` (`productID`);
-
---
--- Beperkingen voor tabel `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `role_ID` FOREIGN KEY (`role_ID`) REFERENCES `role` (`role_ID`);
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
